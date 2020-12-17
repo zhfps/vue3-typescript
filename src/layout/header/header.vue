3e3816f486267aa061edad7804ac032d66f415b7
it_dog <it_dog_zhang@163.com>
2020-12-17 22:41:34 +0800

    2020/12/17

---
+++ src/layout/header/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header-container">
+    <div class="icon">
+         <i :class="icons"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, setup, reactive } from 'vue'
+export default defineComponent({
+  name: 'Header',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    let icons = reactive(props.icon)
+    icons += 'icon'
+    return {
+      icons
+    }
+  }
+})
+</script>
--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -1,62 +1,12 @@
 <template>
 <el-container>
   <el-aside class="aside" style="width: 220px;">
-     <el-menu
-      :uniqueOpened="true"
-      mode="vertical"
-      default-active="2"
-      class="menu"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-submenu index="1">
-        <template #title>
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template #title>选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <template #title>导航二</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <template #title>导航三</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <template #title>导航四</template>
-      </el-menu-item>
-      <el-submenu index="5">
-        <template #title>
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template #title>分组一</template>
-          <el-menu-item index="5-1">选项1</el-menu-item>
-          <el-menu-item index="5-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="5-3">选项3</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-    </el-menu>
+    <Aside/>
   </el-aside>
   <el-container>
-    <el-header class="header" height="44px">Header</el-header>
+    <el-header class="header" height="44px">
+     <Header icon="el-icon-s-fold"/>
+    </el-header>
     <el-main>
        <router-view/>
     </el-main>
@@ -66,8 +16,15 @@
 </template>
 
 <script>
+import { defineComponent } from 'vue'
+import Header from './header/header.vue'
+import Aside from './aside/aside.vue'
 import './layout.scss'
-export default {
-
-}
+export default defineComponent({
+  name: 'layout',
+  components: {
+    Header,
+    Aside
+  }
+})
 </script>
